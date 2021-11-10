5ca5e9b3b8cb8a5963ffed1db7190609cccfaa71
Noam Zilberman <noam@zilberman.org>
2021-11-10 17:42:53 +0200

    email

--- js/cmps-email/email-folders-list.cmp.js
+++ js/cmps-email/email-folders-list.cmp.js
@@ -2,10 +2,24 @@ export default {
     template: `
         <section class="email-folders-list">
             <button class="new-mail"> New Mail</button>
-            <p>Inbox</p>    
-            <p>sent</p>    
-            <p>trash</p>    
-            <p>draft</p>    
+            <ul>
+                <li @click="getType"> Inbox</li>    
+                <li>sent</li>    
+                <p>trash</p>    
+                <p>draft</p>    
+            </ul>
         </section>
     `,
+    data() {
+       return {
+        type: 'inbox'
+       }
+    },
+    methods: {
+        getType(val) {
+            console.log(val);
+            this.type = val 
+        }
+        }
+        //     this.$emit('type', { ...this.filterBy });
 }
--- js/pages/email-app.cmp.js
+++ js/pages/email-app.cmp.js
@@ -8,7 +8,7 @@ export default {
     template: `
         <section v-if="emails" class="email-page">
             <email-filter @filtered="setFilter"/>
-            <email-folders-list />
+            <email-folders-list @type="setType" />
             <email-list :emails="emailsToShow"/>
 
         </section>
@@ -19,8 +19,10 @@ export default {
             filterBy: {
                 title: null,
                 read: null,
-                type: 'inbox'
+                type: 'inbox' //maybe use this instead
             },
+            folders: null,
+            typeSelected: 'sent'
         }
     },
     components: {
@@ -30,6 +32,8 @@ export default {
     },
     created() {
         this.loadEmails()
+        this.setFolders()
+        this.emailsByType()
     },
     methods: {
         loadEmails() {
@@ -44,18 +48,38 @@ export default {
             this.filterBy.title = filterBy.title;
             this.filterBy.read = filterBy.read;
         },
+        setFolders() {
+            this.folders = emailService.getEmailType(this.emails)
+        },
+        emailsByType() {
+            if (this.typeSelected === 'inbox') this.emails = this.folders.inbox
+            else if (this.typeSelected === 'trash') this.emails = this.folders.sent
+        },
+        setType(type) {
+            this.typeSelected = type
+        }
     },
     computed: {
         emailsToShow() {
             if (!this.filterBy.title && !this.filterBy.read) return this.emails
             if (this.filterBy.title) var searchStr = this.filterBy.title.toLowerCase();
-            const emailsToShow = this.emails.filter(email => {
-                console.log(this.emails);
-                console.log(email.isRead)
-                console.log(this.filterBy.isRead)
-                return email.subject.toLowerCase().includes(searchStr) && email.isRead+'' == this.filterBy.read
+            var emailsToShow = this.emails.filter(email => {
+                return email.subject.toLowerCase().includes(searchStr)
             });
+            if (this.filterBy.read && this.filterBy.title) {
+                console.log('im here');
+                var emailsToShow = this.emails.filter(email => {
+                    return email.subject.toLowerCase().includes(searchStr) && email.isRead + '' == this.filterBy.read
+                });
+            }
+            if (this.filterBy.read) {
+                console.log('im hereee');
+                var emailsToShow = this.emails.filter(email => {
+                    return email.isRead + '' == this.filterBy.read
+                });
+            }
             return emailsToShow;
         }
     }
 }
+
