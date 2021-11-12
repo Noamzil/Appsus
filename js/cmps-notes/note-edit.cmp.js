ee345e4607652ba09a01b1112d5ef9d625aed576
eliorpapo <eliorpapo3@gmail.com>
2021-11-12 18:32:22 +0200

    Asd

--- js/cmps-notes/note-edit.cmp.js
+++ js/cmps-notes/note-edit.cmp.js
@@ -1,32 +1,35 @@
 import { noteService } from "../services/note-service.js";
 import noteTxt from "./notes-types.cmp/note-text.cmp.js";
-import noteImg from "./notes-types.cmp/note-img.cmp.js";
+import noteImage from "./notes-types.cmp/note-img.cmp.js";
 import noteTodos from "./notes-types.cmp/note-todo.cmp.js";
 import noteVideo from "./notes-types.cmp/note-video.cmp.js";
 
 export default {
   template: `
-      <section class="note-edit app-main">
-        <form v-if="noteToEdit" @submit.prevent="save" >
-          <h3>Edit the note</h3>
-            <input v-model="noteToEdit.info.title" type="text" placeholder="Title">
-              <template v-if="noteToEdit.type != 'note-todos'" >
-            <input v-if="isUrlHere()" v-model="noteToEdit.info.url" type="text" placeholder="URL">
-            <input v-model="noteToEdit.info.txt" type="text" placeholder="Text">
-          </template>
-          <div class="todo-edit" v-else>
-              <ul>
-                  <li v-for="todo in noteToEdit.info.todos">
-                   <input v-model="todo.txt" type="text" placeholder="Text">
-                  </li>
-              </ul>
-              <button @click="addTodo">Add line</button>
-              </div>
-              <button>Save</button>
-            <component class="note-description-container" :is="noteToEdit.type" :note="noteToEdit"/> </component>
-         </form>
+    <section class="note-edit app-main">
+    <h3>Edit the note</h3>
+      <form class="note-edit-forms" v-if="noteToEdit" @submit.prevent="save" >
+      <div class="edit-lines-container">
+      <input v-model="noteToEdit.info.title" type="text" placeholder="Title">
+      <template v-if="noteToEdit.type != 'note-todos'" >
+      <input v-if="isUrlHere()" v-model="noteToEdit.info.url" type="text" placeholder="URL">
+      <input v-model="noteToEdit.info.txt" type="text" placeholder="Text">
       </template>
-      </section>
+      <div class="todo-edit" v-if="noteToEdit.type === 'note-todos'">
+      <ul>
+      <li v-for="todo in noteToEdit.info.todos">
+      <input v-model="todo.txt" type="text" placeholder="Text">
+      </li>
+      </ul>
+      <button @click="addTodo">Add line</button>
+      </div>
+      </div>
+      <div>
+      <component class="note-description-container" :is="noteToEdit.type" :note="noteToEdit"/> </component>
+      <button class="save-btn">Save</button>
+            </div>
+       </form>
+    </section>
     `,
   data() {
     return {
@@ -60,7 +63,7 @@ export default {
 
   components: {
     noteTxt,
-    noteImg,
+    noteImage,
     noteTodos,
     noteVideo,
     noteService,
